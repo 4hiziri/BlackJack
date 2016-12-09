d9d68e589e6c0a78450300e166eea482c6506a56
4hiziri <meirvg@gmail.com>
2016-12-09 21:35:57 +0900

    use akka-Actor instead of java thread

--- src/main/scala/acceptanceServer/RoomServer.scala
+++ src/main/scala/acceptanceServer/RoomServer.scala
@@ -1,28 +1,33 @@
 package acceptanceServer
 
-import java.io.IOException
-import java.net.{ServerSocket, Socket}
+import java.net.ServerSocket
+
+import akka.actor.{ActorSystem, Props}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 
 object RoomServer {
+  // :TODO extract this setting and include at runtime
   private[serverApp] val ROOM_LIMIT: Int = 10
   private[serverApp] val ENTRY_LIMIT: Int = 10
   private val PORT: Int = 59630
+  private val actor_system = ActorSystem("Room_Server") // :TODO should i extract?
 
-  @SuppressWarnings(Array("resource"))
-  @throws[IOException]
   def main(args: Array[String]) {
-    var sSock: ServerSocket = null // サーバ側のソケット
-    var client_sock: Socket = null // クライアント側のソケット
-    // サーバスタート
-    sSock = new ServerSocket(PORT) // 59630番ポートをサーバとして起動
-    System.out.println("Server Start")
-    while (true) {
-      {
-        client_sock = sSock.accept // クライアントからの接続待ち
-        // スレッドの生成と起動
-        val t: AssignRoomThread = new AssignRoomThread(client_sock)
-        t.start()
+    val sSock: ServerSocket = new ServerSocket(PORT) // 59630番ポートをサーバとして起動
+    val router = {
+      val routees = Vector.fill(5) {
+        val r = context.actorOf(Props[AssignRoomThread])
+        context watch r
+        ActorRefRoutee(r)
       }
+      Router(RoundRobinRoutingLogic(), routees)
+    }
+
+    while (true) {
+      val client_sock = sSock.accept // クライアントからの接続待ち
+      // スレッドの生成と起動
+      // :TODO convert to scala like thread
+      router.route(client_sock, sender())
     }
   }
 }
