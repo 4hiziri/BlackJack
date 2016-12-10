2fb598b2300a8ce5c8238a9585574a834b930164
4hiziri <meirvg@gmail.com>
2016-12-10 11:40:20 +0900

    simplify implement and make actor just in time

--- src/main/scala/acceptanceServer/RoomServer.scala
+++ src/main/scala/acceptanceServer/RoomServer.scala
@@ -3,31 +3,22 @@ package acceptanceServer
 import java.net.ServerSocket
 
 import akka.actor.{ActorSystem, Props}
-import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 
 object RoomServer {
   // :TODO extract this setting and include at runtime
   private[serverApp] val ROOM_LIMIT: Int = 10
   private[serverApp] val ENTRY_LIMIT: Int = 10
   private val PORT: Int = 59630
-  private val actor_system = ActorSystem("Room_Server") // :TODO should i extract?
+  val actor_system = ActorSystem("Room_Server") // :TODO should i extract?
 
   def main(args: Array[String]) {
     val sSock: ServerSocket = new ServerSocket(PORT) // 59630番ポートをサーバとして起動
-    val router = {
-      val routees = Vector.fill(5) {
-        val r = context.actorOf(Props[AssignRoomThread])
-        context watch r
-        ActorRefRoutee(r)
-      }
-      Router(RoundRobinRoutingLogic(), routees)
-    }
+    val props = Props(classOf[AssignRoomThread])
 
     while (true) {
       val client_sock = sSock.accept // クライアントからの接続待ち
       // スレッドの生成と起動
-      // :TODO convert to scala like thread
-      router.route(client_sock, sender())
+      actor_system.actorOf(props, name = "AssignRoomThread") ! client_sock
     }
   }
 }
