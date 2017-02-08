4eb47e48be60bcfc3a596fd82d96ac1904875e14
4hiziri <meirvg@gmail.com>
2017-02-09 00:25:23 +0900

    add Room abstract class, this is Game Logic API

---
+++ src/main/scala/gameServer/Room.scala
@@ -0,0 +1,35 @@
+package gameServer
+
+import acceptanceServer.Client
+import akka.actor.Actor
+
+/**
+  * Created by seiya on 2017/02/09.
+  *
+  * implement game logic
+  * if client is sent, call join(client) and send Check to itself
+  * if Play is sent, call play and send Check to itself
+  * if Check is sent, call check() and if it return true, call notify.
+  * if not, send Play() to itself
+  *
+  * check() - check() all whether game finished or not, implement how to check game state is end here
+  * notify() - notify() players game result, implement how to notify players here
+  * join(Client) - join(Client) adds player to game, implement how to add players to game here
+  * play() - play() run game process, implement how to play game here and recommend processing concurrent
+  */
+abstract class Room extends Actor {
+  override def receive: Receive = {
+    case client: Client => join(client); self ! Check()
+    case _: Check => if (check()) notify() else self ! Play()
+    case _: Play => play(); self ! Check()
+    case _ =>
+  }
+
+  def check(): Boolean
+
+  def notify(): Unit
+
+  def join(client: Client): Unit
+
+  def play(): Unit
+}
