10ec8c2f0a266375808fed53e4cf4a42713a8f0d
4hiziri <meirvg@gmail.com>
2017-02-09 13:26:37 +0900

    add Suit Enum to Card class

--- src/main/scala/gameServer/Card.scala
+++ src/main/scala/gameServer/Card.scala
@@ -4,3 +4,23 @@ import gameServer.Suit.Suit
 
 // :TODO singleton and make playing card set
 case class Card(val number: Int, val suit: Suit)
+
+object Card {
+
+  sealed abstract class Suit
+
+  object Suit {
+
+    case object Spade extends Suit
+
+    case object Club extends Suit
+
+    case object Heart extends Suit
+
+    case object Diamond extends Suit
+
+    case object Jorker extends Suit
+
+  }
+
+}
